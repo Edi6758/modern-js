9358b80fd38476f3658b5e854669da239de5b361
Edi6758 <ediarts7@gmail.com>
2023-03-10 09:20:00 -0300

    finish

---
+++ refatoring-promises/script.js
@@ -0,0 +1,19 @@
+// ...
+
+async function showImc(weight, height) {
+    try {
+      console.log(`Calculando o IMC para peso ${weight} e altura ${height}...`)
+  
+      const result = await imc(weight, height)
+      
+      console.log(`O resultado do IMC foi de ${result}.`)
+    
+      if (result < 18.5) console.log('Situação: MAGREZA')
+      else if (result < 25) console.log('Situação: NORMAL')
+      else if (result < 30) console.log('Situação: SOBREPESO')
+      else if (result < 40) console.log('Situação: OBESIDADE')
+      else console.log('Situação: OBESIDADE GRAVE')
+    } catch (error) {
+      console.log(error)
+    }
+  }
