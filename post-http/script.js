ada5db84ce7afbe066b64a5077fca3a4aa79ff3a
Edi6758 <ediarts7@gmail.com>
2023-03-18 09:26:38 -0300

    update script

--- post-http/script.js
+++ post-http/script.js
@@ -28,7 +28,7 @@ function renderArticle(articleData) {
     fetchArticles()
   })
 
-  const form = document.querySelector('form')
+const form = document.querySelector('form')
 
 form.addEventListener('submit', async (ev) => {
   ev.preventDefault()
